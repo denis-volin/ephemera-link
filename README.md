9fdf7e315a82f980d7fef8a69785653fc6d3dec5
Denis Volin <denis.volin@mail.ru>
2025-07-12 15:59:39 +0300

    feat: implement simple api for creating and retrieving secrets

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Simple web app for creating encrypted secrets that can be viewed only once via u
 - All in one Go binary.
 - System light/dark theme.
 - Unique link to a secret with a configurable length.
+- Simple API for creating and retrieving secrets.
 - The data can be stored in memory or in a database file.
 - The data is encrypted with the AES-256 algorithm.
 
@@ -42,6 +43,36 @@ RUN_CLEARING_INTERVAL=1800
 SECRETS_EXPIRE=86400
 ```
 
+## API
+
+Create secret `/api/create`:
+
+```sh
+$ echo -n "some secret text here" | curl -sSX POST http://localhost:8080/api/create --data-binary @- | jq
+{
+ "expires_at": "2025-07-13T15:55:17+03:00",
+ "expires_in_seconds": "86400",
+ "id": "gzF3tLzP",
+ "key": "CZOzcJU2",
+ "link": "http://localhost:8080/c/gzF3tLzP/CZOzcJU2"
+}
+```
+
+Retrieve secret `/api/retrieve`:
+
+```sh
+# With id and key
+$ echo -e '{
+  "id": "gzF3tLzP",
+  "key": "CZOzcJU2"
+}' | curl -sSX POST http://localhost:8080/api/retrieve --data-binary @-
+some secret text here
+
+# Or with link
+$ curl -sSX POST http://localhost:8080/api/retrieve -d '{"link": "http://localhost:8080/c/gzF3tLzP/CZOzcJU2"}'
+some secret text here
+```
+
 ## Build and run
 
 ```bash
@@ -66,3 +97,8 @@ View page:
 Retrieve page:
 
 ![Retrieve page](screenshots/retrieve.png "Retrieve page")
+
+## TODO
+
+- Add Russian language support.
+- Add dark theme screenshots.
--- app.go
+++ app.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +15,12 @@ type App struct {
 	cfg     *Config
 	storage *Storage
 	r       *gin.Engine
+}
+
+type Retrieve struct {
+	ID   string `json:"id"`
+	Key  string `json:"key"`
+	Link string `json:"link"`
 }
 
 func NewApp(cfg *Config, storage *Storage) *App {
@@ -27,6 +36,8 @@ func (a *App) Run() {
 	a.r.GET("/c/:id/:token", a.OpenSecret)
 	a.r.POST("/saved", a.SaveSecret)
 	a.r.POST("/retrieve", a.RetrieveSecret)
+	a.r.POST("/api/create", a.SaveSecretAPI)
+	a.r.POST("/api/retrieve", a.RetrieveSecretAPI)
 	err := a.r.Run(fmt.Sprintf(":%d", a.cfg.ListenPort))
 	if err != nil {
 		log.Fatalf("Can't start server: %v", err)
@@ -81,4 +92,94 @@ func (a *App) RetrieveSecret(c *gin.Context) {
 
 func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	a.r.ServeHTTP(w, r)
+}
+
+func (a *App) SaveSecretAPI(c *gin.Context) {
+	secret, err := c.GetRawData()
+	if err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read request body",
+		})
+		return
+	}
+
+	id, key, err := a.storage.SaveSecret(string(secret))
+	if err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create secret",
+		})
+		return
+	}
+	expire := a.cfg.SecretsExpire
+	expiryDate := time.Now().Add(time.Second * time.Duration(expire))
+
+	c.JSON(http.StatusOK, gin.H{
+		"id":                 id,
+		"key":                key,
+		"expires_in_seconds": fmt.Sprintf("%d", expire),
+		"expires_at":         fmt.Sprint(expiryDate.Format(time.RFC3339)),
+		"link":               a.cfg.URI + "c/" + id + "/" + key,
+	})
+}
+
+func (a *App) RetrieveSecretAPI(c *gin.Context) {
+	var json Retrieve
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read request body",
+		})
+		return
+	}
+
+	if len(json.ID) != 0 && len(json.Key) != 0 {
+		data, err := a.storage.GetSecret(json.ID, json.Key)
+		if err != nil {
+			c.Error(err)
+			c.JSON(500, gin.H{
+				"error": "This secret has already been viewed or the id/key is invalid.",
+			})
+			return
+		}
+
+		c.String(http.StatusOK, data+"\n")
+	} else if len(json.Link) != 0 {
+		link, err := url.Parse(json.Link)
+		if err != nil {
+			c.Error(err)
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Error parsing link",
+			})
+			return
+		} else if link.Scheme+"://"+link.Host+"/" != a.cfg.URI {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Error parsing link",
+			})
+			return
+		}
+
+		segments := strings.Split(strings.Trim(link.Path, "/"), "/")
+		if len(segments) == 3 {
+			id := segments[1]
+			key := segments[2]
+			data, err := a.storage.GetSecret(id, key)
+			if err != nil {
+				c.Error(err)
+				c.JSON(500, gin.H{
+					"error": "This secret has already been viewed or the id/key is invalid.",
+				})
+				return
+			}
+
+			c.String(http.StatusOK, data+"\n")
+		}
+
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Please provide id and key or link",
+		})
+		return
+	}
 }
